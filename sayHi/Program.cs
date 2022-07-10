c7e65521f9a9af8c4c6f0366f639484559829bb1
KirVoz <k.a.voz97@gmail.com>
2022-07-10 14:05:21 +0300

    lag on IKIKIKI

---
+++ sayHi/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Please, enter your name:");
+string username = Console.ReadLine();
+
+if(username == IKIKIKI);
+{
+Console.WriteLine("Hello, handsome! <3");
+}
+else
+{
+    Console.Write("Get lost!");
+    Console.WriteLine(username);
+}
